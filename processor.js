d73c4d78265b8d2739a4cb4cdcbe6a539dcc0114
Edric Adrian Cantu <136949182+EdricCantu@users.noreply.github.com>
2026-02-17 08:55:58 -0600

    I put it in the wrong repo smh

--- processor.js
+++
@@ -1,7 +0,0 @@
-class Proc extends AudioWorkletProcessor{
-  process(inputs, outputs, params){
-    console.log(inputs, outputs, params);//idk what I'm (going to be) doing rn, but i plan to, and you know i plan to when i pull this move
-  }
-}
-
-registerProcessor("processor", Proc);
