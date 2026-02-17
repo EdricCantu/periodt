ceaa76501fd58764e6ca0fc22960a1da0a6b88f2
Edric Adrian Cantu <136949182+EdricCantu@users.noreply.github.com>
2026-02-17 14:09:58 -0600

    Update processor.js

--- processor.js
+++ processor.js
@@ -1,7 +1,29 @@
 class Proc extends AudioWorkletProcessor{
-  process(inputs, outputs, params){
-    console.log(inputs, outputs, params);//idk what I'm (going to be) doing rn, but i plan to, and you know i plan to when i pull this move
+  state = [0,0];
+  process([input]){
+    if(!this.state[0]){//[0,...]
+      g = this.goertzel(input);
+      if(!this.state[1]){//blackened//[0,0]
+        if(g > 0.5){//move to yellow
+          state = [0, 1, performance.now()];
+          postMessage(1)
+        }else{}//still black
+      }else{//yellowed//[0,1,...]
+        if(g <= 0.5){//not continuous, fail
+          state = [0,0];
+          postMessage(3)
+        }else if(performance.now() - state[2] > 4000){//still continuing, pass if time reached
+          postMessage(2);
+          return false;
+        }else{}//continuing, standby if time not reached
+      }
+    }else{//[1,...]
+      
+    }
     return true;
+  }
+  goertzel(samples){
+    
   }
 }
 
