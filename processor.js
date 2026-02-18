61c8ee196ede3bb51121707277adcc31c91aae78
Edric Adrian Cantu <136949182+EdricCantu@users.noreply.github.com>
2026-02-18 09:38:37 -0600

    Update processor.js

--- processor.js
+++ processor.js
@@ -1,6 +1,7 @@
 class Proc extends AudioWorkletProcessor{
   state = [0,0];
   process(inputs){
+    console.log(inputs);
     const input = inputs[0];
     if (!input) return true;
     if(!this.state[0]){//[0,...]
