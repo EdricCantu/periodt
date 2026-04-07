cebe30158b8b995e8df60eba2e6bf89714b391cd
Edric Adrian Cantu <136949182+EdricCantu@users.noreply.github.com>
2026-04-07 10:18:52 -0500

    Update tsync.js

--- tsync.js
+++ tsync.js
@@ -1,9 +1,17 @@
 //Words I cant spell right on the first try: opportunity february benefiting
+endpoints = [
+  "https://time.now/developer/api/ip",
+  "http://worldclockapi.com/api/json/ct/now",
+  
+  //worldtimeapi.org
+  //timeapi.io
+  //timeapi.world
+  //timeanddate.com
+  //timeapi.org
+];
 
-//https://time.now/developer/api/ip
-//worldtimeapi.org
-//timeapi.io
-//timeapi.world
-//timeanddate.com
-//timeapi.org
-//http://worldclockapi.com/api/json/ct/now
+const endpoint = endpoints[0];
+x = performance.now()
+y = await (await fetch(endpoint)).json();
+x = performance.now() - x;
+console.log(y,x)
