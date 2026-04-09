1858baa77866e6314e1d9e5c7a2399a38cbbad92
Edric Adrian Cantu <136949182+EdricCantu@users.noreply.github.com>
2026-04-09 13:09:12 -0500

    Update tsync.js
    Refactor time fetching logic to include offset calculation based on server response.

--- tsync.js
+++ tsync.js
@@ -1,17 +1,27 @@
 //Words I cant spell right on the first try: opportunity february benefiting
-endpoints = [
-  "https://time.now/developer/api/ip",
-  "http://worldclockapi.com/api/json/ct/now",
-  
-  //worldtimeapi.org
-  //timeapi.io
-  //timeapi.world
-  //timeanddate.com
-  //timeapi.org
-];
 
-const endpoint = endpoints[0];
-x = performance.now()
-y = await (await fetch(endpoint)).json();
-x = performance.now() - x;
-console.log(y,x)
+//is this valid for calculating offsets based on a single request (to which multiple outputs will be aggregated for accuracy)
+function getoffset(i){
+  return new Promise((res,rej)=>{
+    var u;
+         if(i == 0) u = "https://time.now/developer/api/ip";
+    else if(i == 1) u = "http://worldclockapi.com/api/json/ct/now";
+    else            rej(new Error("badurl"));
+    const start = performance.now();
+    fetch(u).then(f=>{
+      const current = Date.now();
+      const end = performance.now();
+      const latency = end - start;
+      const error = latency / 2;
+      const adjcurrent = current - error;//estimate what the system time was when the server time was calculated
+      f.json().then(j=>{
+        if(i == 0){
+          const givenservertime = (new Date(j.datetime)).getTime();
+          res({offset: adjcurrent - givenservertime, error});
+        }
+        if(i == 1) res(j);
+      })
+    }).catch(rej)
+  })
+}
+
