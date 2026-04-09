fb8b18bfdf9758e99a3987ef4628d2da502c729a
Edric Adrian Cantu <136949182+EdricCantu@users.noreply.github.com>
2026-04-09 13:33:48 -0500

    Refactor getoffset to getOffset and add aggOffsets
    Refactor getoffset function to getOffset and add aggregation function for offsets.

--- tsync.js
+++ tsync.js
@@ -1,7 +1,8 @@
 //Words I cant spell right on the first try: opportunity february benefiting
 
 //is this valid for calculating offsets based on a single request (to which multiple outputs will be aggregated for accuracy)
-function getoffset(i){
+//
+function getOffset(i){
   return new Promise((res,rej)=>{
     var u;
          if(i == 0) u = "https://time.now/developer/api/ip";
@@ -19,9 +20,24 @@ function getoffset(i){
           const givenservertime = (new Date(j.datetime)).getTime();
           res({offset: adjcurrent - givenservertime, error});
         }
-        if(i == 1) res(j);
+          if(i == 0) res({});
+          if(i == 1) res(j);
       })
     }).catch(rej)
   })
 }
 
+async function aggOffsets(x = 0, totalo = [], totale = []){
+  if(x != 10){
+    const go = await getOffset(0);
+    totalo.push(go.offset);
+    totale.push(go.error);
+    return await aggOffsets(x + 1, totalo, totale);
+  }else{
+    totalo.sort((a, b) => a - b);
+    totale.sort((a, b) => a - b);
+    o = ( totalo[4] + totalo[5] ) / 2;
+    e = ( totale[4] + totale[5] ) / 2;
+    return {o,e}
+  }
+}
